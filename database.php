05c491b67f5d34acecd3234655dec78bc0d4164e
Devon Leedy <leedy308@gmail.com>
2025-07-06 09:50:00 -0500

    Set up database.php

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=book_app';
+$username = 'root';
+$password = ''; // Adjust if you changed WAMP's default
+
+try {
+    $db = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+    $error_message = $e->getMessage();
+    echo "<p>Database Error: $error_message</p>";
+    exit();
+}
+?>
