6b69efdb882c1b7405d94da287c69d5816be6f26
ASebring0715 <amays1116@gmail.com>
2025-07-06 20:18:39 -0500

    Updated re-fixed delete

--- view/book_list_view.php
+++ view/book_list_view.php
@@ -1,7 +1,57 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="../styles.css">
+    <style>
+    table {
+        width: 80%;
+        border-collapse: collapse;
+        margin: 20px auto;
+        font-family: Arial, sans-serif;
+        background-color: #f9f9f9;
+    }
+
+    th, td {
+        border: 1px solid #ddd;
+        padding: 12px;
+        text-align: left;
+    }
+
+    th {
+        background-color:rgb(175, 76, 139);
+        color: white;
+    }
+
+    tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+
+    tr:hover {
+        background-color: #e9e9e9;
+    }
+
+    caption {
+        caption-side: top;
+        font-size: 1.5em;
+        font-weight: bold;
+        margin-bottom: 10px;
+    }
+
+    .center {
+        text-align: center;
+    }
+
+    button {
+        background-color: #ff4d4d;
+        color: white;
+        border: none;
+        padding: 6px 12px;
+        cursor: pointer;
+    }
+
+    button:hover {
+        background-color: #cc0000;
+    }
+</style>
     <title>Book List</title>
 </head>
 <body>
@@ -21,7 +71,7 @@
         <td><?php echo htmlspecialchars($book['author']); ?></td>
         <td><?php echo nl2br(htmlspecialchars($book['review'])); ?></td>
         <td>
-            <form action="." method="post" style="display:inline;">
+            <form action="../controller/book_controller.php" method="post" style="display:inline;">
                 <input type="hidden" name="action" value="delete_book">
                 <input type="hidden" name="book_id" value="<?php echo $book['book_id']; ?>">
                 <input type="hidden" name="genre_id" value="<?php echo $genre_id; ?>">
